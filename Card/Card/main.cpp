f614b979529aa3cf7ce3c1102650f7b3f6b5a05e
MinJing0 <126648760+MinJing0@users.noreply.github.com>
2023-05-29 22:56:32 +0800

    Update main.cpp

--- Card/Card/main.cpp
+++ Card/Card/main.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 #include "Game.h"
-#include "Card.h"
-#include "Player.h"
+#include "CardStyle.h"
 
 int main()
 {
 	Game game;
 	// 顯示歡迎及規則
-	// game.welcome();
-	// game.gamePlay();
-	// game.rule();
+	game.welcome();
+	game.gamePlay();
+	game.rule();
 
 	// 初始化桌面(拿到整副撲克牌)
 	game.createDeck();
@@ -20,4 +19,66 @@ int main()
 	game.Shuffle();
 	// 將牌給3個玩家及莊家(電腦)
 	game.giveCard();
+	
+	CardStyle cs;
+	Card *c = new Card[5];
+	int cStyle;
+	for(int i = 0; i < 3; ++i)
+	{
+		// 重整 cs
+		cs = CardStyle();
+		// 把玩家的牌存到 c
+		c = game.getPlayer(i).getCards();
+		// 把玩家的牌存到 cs
+		cs.setCards(c);
+		
+		// 跑所有的組合
+		cs.generateCombinations();
+		// 判斷玩家最佳牌型及組合
+		cs.checkStyle();
+		// 回傳最佳組合給 c
+		c = cs.getFinal();
+		// 將最佳組合放回玩家手牌
+		game.getPlayer(i).setCards(c);
+		// 將牌型存到玩家物件裡頭
+		game.getPlayer(i).setCstyle(cs.getCstyle());
+	}
+
+	/*
+	 * 將電腦的牌組排成最佳組合
+	 * 設定電腦最佳牌組的組合牌型
+	*/
+	
+	// 重整 cs
+	cs = CardStyle();
+	// 把電腦的牌存到 c
+	c = game.getPC().getCards();
+	// 把電腦的牌存到 cs
+	cs.setCards(c);
+	
+	// 跑所有的組合
+	cs.generateCombinations();
+	// 判斷電腦最佳牌型及組合
+	cs.checkStyle();
+	// 回傳最佳組合給 c
+	c = cs.getFinal();
+	// 將最佳組合放回電腦手牌
+	game.getPC().setCards(c);
+	// 將牌型存到電腦物件裡頭
+	game.getPC().setCstyle(cs.getCstyle());
+
+	for(int i = 0; i < 3; ++i)
+	{
+		c = game.getPlayer(i).getCards();
+		game.playerHandCard(game.getPlayer(i), i, c);
+	}
+
+	c = game.getPC().getCards();
+	game.pcHandCard(game.getPC(), c);
+
+	for(int i = 0; i < 3; ++i)
+	{
+		game.compare(game.getPlayer(i));
+		game.win(game.getPlayer(i), i);
+	}
 }
