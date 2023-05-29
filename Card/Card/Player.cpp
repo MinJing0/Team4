b2d3fb1d287a37103c032809b231329d0260b6e5
MinJing0 <126648760+MinJing0@users.noreply.github.com>
2023-05-29 22:54:33 +0800

    Upload cStyle and win

--- Card/Card/Player.cpp
+++ Card/Card/Player.cpp
@@ -3,14 +3,35 @@
 Player::Player()
 {
     this->cards = new Card[5];
+    win = 0;
 }
 void Player::setCards(Card* cards)
 {
     for(int i = 0; i < 5; ++i)
         this->cards[i] = cards[i];
 }
+
+void Player::setCstyle(int v)
+{
+    cStyle = v;
+}
+
+int Player::getCstyle()
+{
+    return cStyle;
+}
+
 Card* Player::getCards()
 {
     return cards;
 }
 
+bool Player::getWin()
+{
+    return win;
+}
+
+void Player::setWin(bool w)
+{
+    win = w;
+}
