3e94ace7d5e67f1ccca907a4045ddb40a5772ac4
MinJing0 <126648760+MinJing0@users.noreply.github.com>
2023-05-29 22:49:45 +0800

    Change type double to Card

--- Card/Card/CardStyle.h
+++ Card/Card/CardStyle.h
@@ -1,13 +1,15 @@
+#include "Card.h"
+
 #ifndef Sty_H
 #define Sty_H
 
 class CardStyle
 {
 private:
-    double cards[5];
-    double buffer[5]; // 儲存組合的陣列
-    double final[5]; // 儲存最後找到的最好牌
-    double arr[10][5]; // 儲存所有牌的組合
+    Card cards[5];
+    Card buffer[5]; // 儲存組合的陣列
+    Card final[5]; // 儲存最後找到的最好牌
+    Card arr[10][5]; // 儲存所有牌的組合
     int combinationSize; // 挑選的牌數
     int MAX_COMBINATIONS; // 最大組合數
     int arrIndex;
@@ -23,11 +25,12 @@ private:
 public:
     CardStyle();
     int getCombinationSize() const;
-    void setCards(double []);
-    void generateCombinations(int, int);
+    void setCards(Card []);
+    void generateCombinations();
     void checkStyle(); // 檢查牌型屬於何者
-    double* getFinal();
+    Card* getFinal();
     int getCstyle();
+    Card* getCards();
 
     // test
     void print();         
