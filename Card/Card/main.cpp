7385df47db5851d1d00e212413f626f12057b9c2
MinJing0 <126648760+MinJing0@users.noreply.github.com>
2023-05-29 10:58:14 +0800

    Update main.cpp

--- Card/Card/main.cpp
+++ Card/Card/main.cpp
@@ -5,12 +5,19 @@ using namespace std;
 #include "Card.h"
 #include "Player.h"
 
-//int main()
-//{
-//	Game game;
-//	game.welcome();
-//	game.gamePlay();
-//	game.rule();
-//	game.createDeck();
-//	game.Shuffle();
-//}
+int main()
+{
+	Game game;
+	// 顯示歡迎及規則
+	// game.welcome();
+	// game.gamePlay();
+	// game.rule();
+
+	// 初始化桌面(拿到整副撲克牌)
+	game.createDeck();
+
+	// 洗牌
+	game.Shuffle();
+	// 將牌給3個玩家及莊家(電腦)
+	game.giveCard();
+}
